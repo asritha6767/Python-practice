3b30ae3851e7a9ff6f54a3f99ee6a159d8e3c7b4
asritha6767 <asritha6767@gmail.com>
2026-08-19 12:52:47 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,9 @@
+
+# Day 7: While Loop + User Input
+print("Calculator")
+
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+
+print("Sum:", num1 + num2)
+print("Multiply:", num1 * num2)
