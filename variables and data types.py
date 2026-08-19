da82be25fcca47479ea442cf025840b8ffd308f5
asritha6767 <asritha6767@gmail.com>
2026-08-19 12:38:54 +0530

    Add files via upload

---
+++ variables and data types.py
@@ -0,0 +1,9 @@
+
+# Day 1: Variables and Data Types
+name = "Asritha"
+age = 21
+is_learning_python = True
+
+print("My name is", name)
+print("My age is", age)
+print("Learning Python:", is_learning_python)
